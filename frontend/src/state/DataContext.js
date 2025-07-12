6c923dc4ff22addc6c59ad3a702117f013e9ef55
arek <codingdayandnight@gmail.com>
2025-07-12 15:48:41 +0200

    fix memory leak

--- frontend/src/state/DataContext.js
+++ frontend/src/state/DataContext.js
@@ -5,10 +5,21 @@ const DataContext = createContext();
 export function DataProvider({ children }) {
   const [items, setItems] = useState([]);
 
-  const fetchItems = useCallback(async () => {
-    const res = await fetch('http://localhost:3001/api/items?limit=500'); // Intentional bug: backend ignores limit
-    const json = await res.json();
-    setItems(json);
+  const fetchItems = useCallback(async (controller) => {
+
+    // simulate 2 sec request duration
+    setTimeout(async () => {
+      let parsedJson
+
+      try {
+        const res = await fetch('http://localhost:3001/api/items?limit=500', { signal: controller.signal }); // Intentional bug: backend ignores limit
+        parsedJson = await res.json();
+        setItems(parsedJson);
+        console.log('following items have been set:', parsedJson);
+      } catch {
+        console.warn('request interrupted. following items have not been set:', parsedJson);
+      }
+    }, 2000);
   }, []);
 
   return (
