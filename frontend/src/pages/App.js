07479243a38036486b3274b8580f495813f402c4
arek <codingdayandnight@gmail.com>
2025-07-12 15:51:32 +0200

    add /unmount to react router

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -9,10 +9,13 @@ function App() {
     <DataProvider>
       <nav style={{padding: 16, borderBottom: '1px solid #ddd'}}>
         <Link to="/">Items</Link>
+        <br/>
+        <Link to="/unmount">Unmount Items.js while items are loading</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Items />} />
         <Route path="/items/:id" element={<ItemDetail />} />
+        <Route path="/unmount" element={<h1>Items.js unmounted</h1>} />
       </Routes>
     </DataProvider>
   );
